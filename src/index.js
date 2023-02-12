5d3a86bffc596e505b9fd0bca6d347da51486f98
KimYaroslav <Korean_mafia_01@mail.ru>
2023-02-13 00:06:19 +0600

    feat: add task solution in js file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const splitExpr = [];
+  let transExpr = [];
+  for (let i = 0; i < expr.length; i += 2) {
+      const chunk = expr.slice(i, i + 2);
+      splitExpr.push(chunk);
+  }
+
+  for (let i = 0; i < splitExpr.length; i += 5) {
+    const chunk = splitExpr.slice(i, i + 5);
+    transExpr.push(chunk);
+  }
+
+  for (let i = 0; i < transExpr.length; i++) {
+    for (let b = 0; b < transExpr[i].length; b++) {
+      if (transExpr[i][b] === '00') {transExpr[i][b] = ''};
+      
+      if (transExpr[i][b] === '10') {transExpr[i][b] = '.'};
+
+      if (transExpr[i][b] === '11') {transExpr[i][b] = '-'};
+    }
+  }
+
+  for (let i = 0; i < transExpr.length; i++) {
+    transExpr[i] = transExpr[i].join('');
+    if (transExpr[i] === '**********') {transExpr[i] === ' '};
+  }
+  let result = transExpr.map((value) => MORSE_TABLE[value] !== undefined ? MORSE_TABLE[value] : ' ').join('').toString('');
+
+  return result;
+
 }
 
 module.exports = {
